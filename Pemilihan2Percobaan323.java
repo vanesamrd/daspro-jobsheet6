0b27ca835358842098a7235f5ad42a3eea077e4d
vanesamrd <paneja435@gmail.com>
2024-10-02 11:51:07 +0700

    feat modifikasi baru

--- Pemilihan2Percobaan323.java
+++ Pemilihan2Percobaan323.java
@@ -16,7 +16,7 @@ public class Pemilihan2Percobaan323 {
         System.out.println("Masukkan Besarnya Penghasilan : ");
         penghasilan = input23.nextInt();
 
-        if (kategori.equalsIgnoreCase("pekerja")){
+        if (kategori.equals("pekerja")){
             if (penghasilan <= 2000000)
                 pajak = 0.1;
             else if (penghasilan <= 3000000)
@@ -25,7 +25,7 @@ public class Pemilihan2Percobaan323 {
                 pajak = 0.2;
             gajiBersih = (int) (penghasilan - (pajak * penghasilan));
             System.out.println("Penghasilan Bersih : " + gajiBersih);
-        }else if (kategori.equalsIgnoreCase("pebisnis")) {
+        }else if (kategori.equals("pebisnis")) {
             if (penghasilan <= 2500000)
                 pajak = 0.15;
             else if (penghasilan <= 3500000)
