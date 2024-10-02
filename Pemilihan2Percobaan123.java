0d41a3b3c99e6d58ffec206861147d61e91d01cf
vanesamrd <paneja435@gmail.com>
2024-10-02 09:58:07 +0700

    feat terbaru

--- Pemilihan2Percobaan123.java
+++ Pemilihan2Percobaan123.java
@@ -8,10 +8,17 @@ public class Pemilihan2Percobaan123 {
         Scanner input23 = new Scanner(System.in);
         int tahun = input23.nextInt();
 
-        if((tahun % 4) == 0){
-            if((tahun % 100) != 0)
+        if ((tahun % 4) == 0) {
+            if ((tahun % 100) != 0){
                 System.out.println("Tahun Kabisat");
-        } else 
+            } else if((tahun % 100) != 0) {
+                System.out.println("Tahun Kabisat");
+            } else {
+                System.out.printf("Tahun %s Bukan Tahun Kabisat", tahun);
+            }
+         } else {
             System.out.println("Bukan Tahun Kabisat");
+         }
+
     }
 }
